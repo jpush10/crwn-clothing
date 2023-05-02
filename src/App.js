ee410c6f2be1558895e11bff3396742b488728a7
Piyush Joshi <jpush10@gmail.com>
2023-05-02 08:20:18 +0530

    Initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from './routes/home/home.component';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />}>
+        
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
