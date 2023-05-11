43b9acd1aafc1f68d0ad030d51efa6027958c277
crestpiyush <piyush.j@crestinfosystems.net>
2023-05-11 18:31:37 +0530

    Button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+import './sign-up-form.styles.scss';
+import Button from "../button/button.component";
 
 const defaultFormFields = {
     displayName: "",
@@ -43,8 +45,9 @@ const SignUpForm = () => {
         setFormFields({...formFields, [name]: value});
     }
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 {/* <label htmlFor="">Display Name</label> */}
                 <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName}/>
@@ -58,7 +61,7 @@ const SignUpForm = () => {
                 {/* <label htmlFor="">Confirm Password</label> */}
                 <FormInput  label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
 
-                <button type="submit" >Sign Up</button>
+                <Button buttonType='default' type="submit" >Sign Up</Button>
             </form>
         </div>
     )
